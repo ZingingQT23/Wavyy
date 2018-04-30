ceb7521afe0e234161403c7b8a2e81133ce01c9a
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-04-30 17:26:34 -0400

    Created signup controller

---
+++ Wavyy/Wavyy/Controllers/SignupController.cs
@@ -0,0 +1,32 @@
+﻿using Wavyy.Models;
+using Wavyy.ViewModels;
+using System.Web.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Wavyy.Controllers
+{
+    public class SignupController : Controller
+    {
+        private WavyyDbContext context;
+
+        public SignupController(WavyyDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(NewUserViewModel newUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+            return View();
+        }
+    }
+}
