755164f074f7f489c2ac889e2ffe4b82b1ff2170
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-04-30 17:29:10 -0400

    Created newuserviewmodel, inheriting from the user class

---
+++ Wavyy/Wavyy/ViewModels/NewUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Wavyy.Models;
+
+namespace Wavyy.ViewModels
+{
+    public class NewUserViewModel : User
+    {
+        public string VPassword { get; set; }
+        public string VEmail { get; set; }
+    }
+}
