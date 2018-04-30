2d176a3bdebed24a66c14af1e7f095ea68ed15b4
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-04-30 17:26:55 -0400

    Created the user class

---
+++ Wavyy/Wavyy/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wavyy.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
