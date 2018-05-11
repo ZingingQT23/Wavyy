92db46947a97a14d99ee8ea77f1d0099a47995e0
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-11 14:29:40 -0400

    Created a Model for user searches

---
+++ Wavyy/Wavyy/Models/SearchTerms.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wavyy.Models
+{
+    public class SearchTerms
+    {
+        [Required]
+        public string UserInput { get; set; }
+    }
+}
