f626c18e374fc078608fd5d05e7eb88b45bcfba3
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-17 17:35:44 -0400

    Created the Game object class for storing games in the local database

---
+++ Wavyy/Wavyy/Models/Games/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Wavyy.Models.Games
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        public int DbId { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Summary { get; set; }
+        public GameCover Cover { get; set; }
+        public List<int> Platforms { get; set; }
+    }
+}
