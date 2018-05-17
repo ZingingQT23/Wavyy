ae7efc1f3ee1679987dae6c69c5b2697bc7117ef
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-17 17:36:59 -0400

    Created a GameCover object to store the location and information about each Game object's cover image

---
+++ Wavyy/Wavyy/Models/Games/GameCover.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Wavyy.Models.Games
+{
+    public class GameCover
+    {
+        [JsonProperty(PropertyName = "url")]
+        public string Url { get; set; }
+        [JsonProperty(PropertyName = "cloudinary_id")]
+        public string CloudinaryId { get; set; }
+        [JsonProperty(PropertyName = "width")]
+        public int Width { get; set; }
+        [JsonProperty(PropertyName = "height")]
+        public int Height { get; set; }
+    }
+}
