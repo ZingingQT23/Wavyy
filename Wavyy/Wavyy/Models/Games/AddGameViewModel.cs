2d46fef65ea17805e11aa1295745aa6dbd4ea25c
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-17 17:35:12 -0400

    Created the view model for adding a game to the local database

---
+++ Wavyy/Wavyy/Models/Games/AddGameViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Wavyy.Models.Games
+{
+    public class AddGameViewModel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int ID { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "slug")]
+        public string Slug { get; set; }
+        [JsonProperty(PropertyName = "summary")]
+        public string Summary { get; set; }
+        [JsonProperty(PropertyName = "cover")]
+        public GameCover Cover { get; set; }
+        [JsonProperty(PropertyName = "platforms")]
+        public List<int> Platforms { get; set; }
+    }
+}
