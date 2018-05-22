76ed139453b46ad6931544e095fb48be40f8357b
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-22 15:11:59 -0400

    Added UserCollections, which is a list of Collection objects

--- Wavyy/Wavyy/Models/ApplicationUser.cs
+++ Wavyy/Wavyy/Models/ApplicationUser.cs
@@ -1,13 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+using Wavyy.Models.Games;
 
 namespace Wavyy.Models
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public List<Collection> UserCollections { get; set; }
     }
 }
