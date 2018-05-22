ffad454155876b1af4c880ab0faf68bc15ed9751
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-22 15:07:57 -0400

    Created the Collection model, which for now is just a list of Game objects

---
+++ Wavyy/Wavyy/Models/Games/Collection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Wavyy.Models.Games
+{
+    public class Collection
+    {
+        public int ID { get; set; }
+        public List<Game> Games { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
