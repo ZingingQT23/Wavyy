75d12afee64072c3d3017246fa954bcce29446ac
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-22 15:10:48 -0400

    Changed DbId to a DbId object type and added the neccessary constructors

--- Wavyy/Wavyy/Models/Games/Game.cs
+++ Wavyy/Wavyy/Models/Games/Game.cs
@@ -5,11 +5,24 @@ namespace Wavyy.Models.Games
     public class Game
     {
         public int ID { get; set; }
-        public int DbId { get; set; }
+        public DbId DbId { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
         public string Summary { get; set; }
         public GameCover Cover { get; set; }
         public List<int> Platforms { get; set; }
+
+        public Game() { }
+
+        public Game(AddGameViewModel addGameViewModel)
+        {
+            DbId = new DbId(addGameViewModel.DbId);
+            Name = addGameViewModel.Name;
+            Slug = addGameViewModel.Slug;
+            Summary = addGameViewModel.Summary;
+            Cover = addGameViewModel.Cover;
+            Platforms = addGameViewModel.Platforms;
+        }
+
     }
 }
