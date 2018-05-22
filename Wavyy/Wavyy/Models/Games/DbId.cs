957391e0cea197ba95c670515b01af7457295b89
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-22 15:09:52 -0400

    Created DbId as a game object so that they can be deserialized and iterated through

---
+++ Wavyy/Wavyy/Models/Games/DbId.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Wavyy.Models.Games
+{
+    [JsonObject]
+    public class DbId : IEnumerable<int>
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        public DbId(int id) { Id = id; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return Id;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield return GetEnumerator();
+        }
+    }
+}
