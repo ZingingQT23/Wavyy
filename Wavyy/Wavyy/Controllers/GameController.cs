425632f28aa73558a3b6f1aaae8965365aa999f9
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-30 15:16:55 -0400

    Created the Game controller for managing Games and Collections

---
+++ Wavyy/Wavyy/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wavyy.Data;
+
+namespace Wavyy.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly WavyyDbContext context;
+
+        public GameController(WavyyDbContext dbContext)
+        {
+            context = dbContext;
+        }
+    
+        public IActionResult MyGames()
+        {
+            //TODO: Search context for the active user and its UserGames list and pass it into the view
+
+            return View();
+        }
+
+
+    }
+}
