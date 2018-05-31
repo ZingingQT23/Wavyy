4dacba90f92bbd5c9bcaee331dde32dfc25cb760
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-31 16:15:44 -0400

    Created a game object for the esrb and pegi ratings

---
+++ Wavyy/Wavyy/Models/Games/Child Objects/Rating.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Wavyy.Models.Games.Child_Objects
+{
+    [JsonObject]
+    public class Rating
+    {
+        [JsonProperty(PropertyName = "rating")]
+        public int RatingId { get; set; }
+        [JsonProperty(PropertyName = "synopsis")]
+        public string Synopsis { get; set; }
+    }
+}
