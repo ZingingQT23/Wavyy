67d49c8ab3898a22e8dc49340ae677fc52370a32
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-05-31 16:20:45 -0400

    Added new fields for information about each Game

--- Wavyy/Wavyy/Models/Games/AddGameViewModel.cs
+++ Wavyy/Wavyy/Models/Games/AddGameViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using Wavyy.Models.Games.Child_Objects;
 
 namespace Wavyy.Models.Games
 {
@@ -15,8 +16,26 @@ namespace Wavyy.Models.Games
         [JsonProperty(PropertyName = "summary")]
         public string Summary { get; set; }
         [JsonProperty(PropertyName = "cover")]
-        public GameCover Cover { get; set; }
+        public GameImage Cover { get; set; }
         [JsonProperty(PropertyName = "platforms")]
         public List<int> Platforms { get; set; }
+        //[JsonProperty(PropertyName = "first_release_date")]
+        //public int FirstReleaseDate { get; set; }
+        [JsonProperty(PropertyName = "storyline")]
+        public string Storyline { get; set; }
+        [JsonProperty(PropertyName = "esrb")]
+        public Rating Esrb { get; set; }
+        [JsonProperty(PropertyName = "pegi")]
+        public Rating Pegi { get; set; }
+        [JsonProperty(PropertyName = "screenshots")]
+        public List<GameImage> Screenshots { get; set; }
+        [JsonProperty(PropertyName = "artworks")]
+        public List<GameImage> Artworks { get; set; }
+        [JsonProperty(PropertyName = "genres")]
+        public List<int> GenreIds { get; set; }
+        [JsonProperty(PropertyName = "developers")]
+        public List<int> DeveloperIds { get; set; }
+        [JsonProperty(PropertyName = "publishers")]
+        public List<int> PublisherIds { get; set; }
     }
 }
--- Wavyy/Wavyy/Models/Games/Game.cs
+++ Wavyy/Wavyy/Models/Games/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Wavyy.Models.Games.Child_Objects;
 
 namespace Wavyy.Models.Games
 {
@@ -9,8 +10,19 @@ namespace Wavyy.Models.Games
         public string Name { get; set; }
         public string Slug { get; set; }
         public string Summary { get; set; }
-        public GameCover Cover { get; set; }
+        public string Storyline { get; set; }
+        public Rating Esrb { get; set; }
+        public Rating Pegi { get; set; }
+        //public int FirstReleaseDate { get; set; }
+        public GameImage Cover { get; set; }
         public List<int> Platforms { get; set; }
+        public List<GameImage> Screenshots { get; set; }
+        public List<GameImage> Artworks { get; set; }
+        public List<int> GenreIds { get; set; }
+        public List<int> DeveloperIds { get; set; }
+        public List<int> PublisherIds { get; set; }
+        public List<int> UserCollectionIds { get; set; }
+
 
         public Game() { }
 
@@ -22,10 +34,19 @@ namespace Wavyy.Models.Games
             Summary = addGameViewModel.Summary;
             Cover = addGameViewModel.Cover;
             Platforms = addGameViewModel.Platforms;
+            //FirstReleaseDate = addGameViewModel.FirstReleaseDate;
+            Storyline = addGameViewModel.Storyline;
+            Esrb = addGameViewModel.Esrb;
+            Pegi = addGameViewModel.Pegi;
+            Screenshots = addGameViewModel.Screenshots;
+            Artworks = addGameViewModel.Artworks;
+            GenreIds = addGameViewModel.GenreIds;
+            DeveloperIds = addGameViewModel.DeveloperIds;
+            PublisherIds = addGameViewModel.PublisherIds;
           
             if (Cover == null)
             {
-                Cover = new GameCover();
+                Cover = new GameImage();
                 Cover.Url = "";
             }
         }
