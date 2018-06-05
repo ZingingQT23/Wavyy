e3bee552ed092fa8e977dd30c7f9ae685919d563
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-06-05 16:47:10 -0400

    Removed unnecessary properties

--- Wavyy/Wavyy/Models/ApplicationUser.cs
+++ Wavyy/Wavyy/Models/ApplicationUser.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
-using Wavyy.Models.Games;
 
 namespace Wavyy.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        //public List<Collection> UserCollections { get; set; }
-        //public List<UserGame> UserGames { get; set; }
     }
 }
