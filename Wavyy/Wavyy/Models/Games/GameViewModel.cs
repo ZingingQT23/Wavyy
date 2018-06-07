1ab5a19191467c6e59e1c33ac93900ec11820e03
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-06-07 15:28:56 -0400

    Created the GameViewModel for multipurpose use

---
+++ Wavyy/Wavyy/Models/Games/GameViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Wavyy.Models.Games
+{
+    public class GameViewModel : AddGameViewModel
+    {
+        public int GameID { get; set; }
+        public string UserID { get; set; }
+        public int PlatformId { get; set; }
+        public int VersionId { get; set; }
+
+        public GameViewModel(UserGame userGame)
+        {
+            GameID = userGame.GameID;
+            UserID = userGame.UserID;
+            PlatformId = userGame.PlatformId;
+            VersionId = userGame.VersionId;
+        }
+
+        public GameViewModel(AddGameViewModel addGameViewModel)
+        {
+            Name = addGameViewModel.Name;
+            Slug = addGameViewModel.Slug;
+            Summary = addGameViewModel.Summary;
+            Storyline = addGameViewModel.Storyline;
+            Cover = addGameViewModel.Cover;
+            Artworks = addGameViewModel.Artworks;
+            Screenshots = addGameViewModel.Screenshots;
+
+        }
+    }
+}
