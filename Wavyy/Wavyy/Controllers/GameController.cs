ad694126c9e27eaa27277116b54e5112aee812fd
Dylan Kenyon <dylan.dean.kenyon@gmail.com>
2018-06-07 15:30:13 -0400

    Created the My Games funtion

--- Wavyy/Wavyy/Controllers/GameController.cs
+++ Wavyy/Wavyy/Controllers/GameController.cs
@@ -5,6 +5,8 @@ using Wavyy.Data;
 using Wavyy.Models;
 using Wavyy.Models.Games;
 using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Wavyy.Controllers
 {
@@ -17,11 +19,42 @@ namespace Wavyy.Controllers
             context = dbContext;
         }
 
-
-    
-        public IActionResult MyGames()
+        public IActionResult SingleGame(int id)
         {
-            //TODO: Search context for the active user and its UserGames list and pass it into the view
+            Game thisGame = context.Games.Where(x => x.ID == id).FirstOrDefault<Game>();
+
+            TempData["ThisGame"] = thisGame;
+
+            return View();
+        }
+
+        public async Task<IActionResult> MyGames()
+        {
+            string userId = User.Identity.GetUserId();
+
+            List<UserGame> userGames = context.UserGames.Where(x => x.UserID == userId).ToList();
+
+            List<GameViewModel> myGames = new List<GameViewModel>();
+
+            foreach (UserGame game in userGames)
+            {
+                GameViewModel userGameViewModel = new GameViewModel(game);
+
+                //var cover = context.GameImages.Where(x => x.GameID == userGameViewModel.GameID).Where(x => x.Type == "cover").FirstOrDefault().Url;
+
+                //if (cover != null)
+                //{
+                //    string coverUrl = cover.ToString();
+                //    userGameViewModel.CoverUrl = coverUrl;
+                //} else
+                //{
+                //    userGameViewModel.CoverUrl = "";
+                //}
+
+                myGames.Add(userGameViewModel);
+            }
+
+            TempData["myGames"] = myGames;
 
             return View();
         }
@@ -45,7 +78,7 @@ namespace Wavyy.Controllers
             context.UserGames.Add(newUserGame);
             context.SaveChanges();
 
-            return View("/Game/MyGames");
+            return Redirect("/Home/Index");
         }
     }
 }
